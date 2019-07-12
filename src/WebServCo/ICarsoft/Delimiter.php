5c2c9fdf53c1adee7b900cb4b53d5e3c9c1976d2
Radu Maier <ariademur@webserv.co>
2019-07-12 17:02:23 +0200

    delimiter

---
+++ src/WebServCo/ICarsoft/Delimiter.php
@@ -0,0 +1,22 @@
+<?php
+namespace WebServCo\ICarsoft;
+
+final class Delimiter
+{
+    const LINE = "\n";
+
+    const HEADER_SECTION = '========================================================================';
+    const HEADER_DATA = ':';
+
+    const TITLE_SECTION = "\n";
+    const TITLE_DATA = '>';
+
+    const INFO_ITEMS = ',';
+    const INFO_DATA = ':';
+
+    const CONTENT_DATA = ':';
+
+    const FRAME_SECTION = '- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - '.
+    '- - - - - - - - - - - - - - - - - - - - - - -';
+    const FRAME_DATA = '-----';
+}
